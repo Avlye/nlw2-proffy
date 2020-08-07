5fd51def799979e7debcd88a8abb2e3a58a05eae
Gabriel Almir <avlye@outlook.com>
2020-08-07 19:24:13 -0300

    Add: Initialize Landing Page

---
+++ packages/mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+import { styles } from './styles'
+
+export function Landing() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
