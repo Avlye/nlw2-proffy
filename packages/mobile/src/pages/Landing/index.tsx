f954b0a94faafcd4125b51e8f95bc45005d74cf1
Gabriel Almir <avlye@outlook.com>
2020-08-07 19:40:08 -0300

    Add: google fonts

--- packages/mobile/src/pages/Landing/index.tsx
+++ packages/mobile/src/pages/Landing/index.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 
-import { View, Text } from "react-native";
+import { AppLoading } from "expo";
+import { View, Text, Image } from "react-native";
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
 
 import { styles } from './styles'
 
+import landingImg from '../../assets/images/landing.png'
+
 export function Landing() {
-  return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  )
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular, Archivo_700Bold,
+    Poppins_400Regular, Poppins_600SemiBold
+  })
+
+  if (!fontsLoaded) return <AppLoading />
+  else {
+    return (
+      <View style={styles.container}>
+        <Image source={landingImg} style={styles.banner} />
+
+        <Text style={styles.title}>
+          Seja bem-vindo, {'\n'}
+
+          <Text style={styles.titleBold}>
+            O que deseja fazer?
+          </Text>
+        </Text>
+      </View>
+    )
+  }
 }
