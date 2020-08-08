b1f3789e1a8f8be119ddd5d1644657b9d1828c36
Gabriel Almir <avlye@outlook.com>
2020-08-08 17:54:44 -0300

    Add: Finish Lessons 4

--- packages/mobile/src/pages/TeacherList/index.tsx
+++ packages/mobile/src/pages/TeacherList/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { View } from "react-native";
 
+import { styles } from "./styles";
+import { PageHeader } from "../../components/PageHeader";
+
 export function TeacherList() {
-  return <View></View>
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponíveis" />
+    </View>
+  )
 }
